4087c93cfa05858f2ed67ed676a30de265541396
saeki <maruponn256@gmail.com>
2022-12-27 20:39:34 +0900

    テストを通りません

--- src/StaticLib/StaticLib.c
+++ src/StaticLib/StaticLib.c
@@ -1,6 +1,7 @@
 ﻿#define WIN32_LEAN_AND_MEAN             // Windows ヘッダーからほとんど使用されていない部分を除外する
 #include "Windows.h"                    // Windows API の機能定義
-
+#include <stdio.h>
+#include <stdlib.h>
 #include "../include/lib_func.h"
 
 
@@ -9,5 +10,57 @@
 bool radix_sort(item* begin, const item* end)
 {
 	// ToDo: 基数ソートを実装する
-	return false;
+	if (!begin || !end || end - begin < 0)return false;
+
+	if (end - begin < 2)return true;
+
+	//int size = end - begin;
+
+	swap_radix(begin, end - begin, 256);
+
+	return true;
+}
+
+static void swap_radix(item* order, const int num, const int rad)
+{
+	int i, j, count, n;
+	int level = 1, skep = 0;
+	
+	item *copy = (item*)malloc(sizeof(item) * num);
+	int *bottom = (int*)malloc(sizeof(int) * num + 1);
+
+	while (skep != num)
+	{
+		skep = 0;
+
+		for (i = 0; i < num; i++)
+		{
+			n = order[i].key / level;
+			bottom[i] = n % rad;
+
+			if (order[i].key < (unsigned)level)
+				skep++;
+		}
+
+		count = 0;
+
+		for (i = 0; i <= rad; i++)
+		{
+			for (j = 0; j < num; j++)
+			{
+				if (bottom[j] == i)
+					copy[count] = order[j];
+
+				count++;
+			}
+		}
+
+		for (i = 0; i < num; i++)
+			order[i] = copy[i];
+
+		level *= rad;
+	}
+
+	free(copy);
+	free(bottom);
 }
--- src/include/lib_func.h
+++ src/include/lib_func.h
@@ -1,6 +1,8 @@
 ﻿#pragma once // インクルードガード
 
 #include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
 
 // C++ でCのライブラリを使うときのおまじない
 #ifdef __cplusplus
@@ -16,6 +18,8 @@ extern "C" {
 	// 基数ソート(引数が不適切であればfalseを返す)
 	// 基数は256で実装せよ
 	bool radix_sort(item *begin, const item* end);
+
+	void swap_radix(item *order, int num, int rad);
 
 	// C++ でCのライブラリを使うときのおまじない
 #ifdef __cplusplus
