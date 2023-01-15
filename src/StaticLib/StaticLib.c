692ea9f0cd1d70c67bb91bd75fe9b07620028d9c
Harumagedon <g2227059@st.t-kougei.ac.jp>
2023-01-15 22:10:47 +0900

    無駄なコメントの削除

--- src/StaticLib/StaticLib.c
+++ src/StaticLib/StaticLib.c
@@ -1,6 +1,6 @@
 ﻿#define WIN32_LEAN_AND_MEAN             // Windows ヘッダーからほとんど使用されていない部分を除外する
 #include "Windows.h"                    // Windows API の機能定義
-#include "stdlib.h"                    // Windows API の機能定義
+#include "stdlib.h"                    
 
 #include "../include/lib_func.h"
 
